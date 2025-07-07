e3c3839e61b06e937e0c37c7b0215a78c6e8c35f
adumitre <andrewdumitrescu@gmail.com>
2025-07-06 22:18:29 -0500

    Converted to group management for objects

--- main.py
+++ main.py
@@ -6,23 +6,31 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     dt = 0
-
-    player1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     
     # Main Game Loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        updatable.update(dt)
+        
         screen.fill("black")
-        player1.draw(screen)
-        player1.update(dt)
+        
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
+        
         dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
